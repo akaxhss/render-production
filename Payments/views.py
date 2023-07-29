dd831e4c8bdf040d88939cde913ee3fe4a731188
Akash Satheesh <satheeshakash07@gmail.com>
2023-07-29 16:48:54 +0530

    made everything back and edited code 178&101

--- Payments/views.py
+++ Payments/views.py
@@ -71,7 +71,7 @@ def checkout(request):
                     return JsonResponse({"error": "selected plan not found"}, status=status.HTTP_404_NOT_FOUND)
 
                 # ! make the current active plan to inactive
-                # Subscriptions.objects.filter(customer=payment.customer, is_active=True).update(is_active=False)
+                Subscriptions.objects.filter(customer=payment.customer, is_active=True).update(is_active=False)
 
                 # ? new subscription
                 validity = datetime.now() + timedelta(days=membership.validity)  # days=payment.membership.validity
@@ -98,8 +98,8 @@ def checkout(request):
                 # email.send()
 
                 # make is_active false after successfull payment
-                # payment.customer.is_active = True
-                payment.customer.save()
+                # payment.customer.is_active = False
+                # payment.customer.save()
                 # render success page on successful caputre of payment
                 return JsonResponse({"success": "Payment captured successfully.!"}, status=status.HTTP_200_OK)
             else:
@@ -169,14 +169,14 @@ def get_free_subscription(request):
     except MembershipPlans.DoesNotExist:
         return Response({'error': 'membership not found'}, status=status.HTTP_404_NOT_FOUND)
     # ! make the current active plan to inactive
-    Subscriptions.objects.filter(customer=user, is_active=True)#.update(is_active=False)
+    Subscriptions.objects.filter(customer=user, is_active=True).update(is_active=False)
 
     # ? new subscription
     validity = datetime.now() + timedelta(days=membership.validity)
     Subscriptions.objects.create(customer=user, valid_till=make_aware(validity), membership=membership, is_active=True)
     # make account inactive.
     # user.is_active = False
-    user.save()
+    # user.save()
     return Response({'success': 'subscribed to free version'})
 
 
